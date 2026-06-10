57600b3fef88c512e30a7c949f2c9e791cd5cfda
Jerome Chua <jeromechua.jz@gmail.com>
2026-06-10 21:58:02 +0800

    changed lualine look

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ require('lualine').setup({
     options = {
         icons_enabled = true,
         theme = 'palenight',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -21,8 +21,13 @@ require('lualine').setup({
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_c = {
+            {
+                'filename',
+                path = 1
+            }
+        },
+        lualine_x = {'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },

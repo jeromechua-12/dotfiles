8f10e237dc447b644b3e6920144c29f50277c31d
Jerome Chua <jeromechua.jz@gmail.com>
2026-08-18 20:57:25 +0800

    ignore .vevn in fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -7,7 +7,12 @@ require('fzf-lua').setup({
             ["ctrl-k"] = "up",
             ["ctrl-l"] = "accept",
         },
-    }
+    },
+    files = {
+        find_opts         = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*' \! -path '*/.venv/*']],
+        rg_opts           = [[--color=never --files -g "!.git" -g "!.jj" -g "!.venv"]],
+        fd_opts           = [[--color=never --type f --type l --exclude .git --exclude .jj --exclude .venv]],
+    },
 })
 
 -- keybindings

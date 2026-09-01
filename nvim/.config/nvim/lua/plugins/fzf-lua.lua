e990381bc29d00b1ca1d602030c6cce31083f145
Jerome Chua <jeromechua.jz@gmail.com>
2026-09-02 00:23:20 +0800

    enable files in .gitignore in fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -10,8 +10,8 @@ require('fzf-lua').setup({
     },
     files = {
         find_opts         = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*' \! -path '*/.venv/*']],
-        rg_opts           = [[--color=never --files -g "!.git" -g "!.jj" -g "!.venv"]],
-        fd_opts           = [[--color=never --type f --type l --exclude .git --exclude .jj --exclude .venv]],
+        rg_opts           = [[--color=never --files --no-ignore -g "!.git" -g "!.jj" -g "!.venv"]],
+        fd_opts           = [[--color=never --type f --type l --no-ignore --exclude .git --exclude .jj --exclude .venv]],
     },
 })
 
